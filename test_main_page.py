cdc5eb9b82445c457ca8114b021a214c982ec7dc
Sergey Silenok <silenoksergey34@gmail.com>
2023-04-02 13:05:03 +0500

    added tests of the registration and authorization page

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,16 @@
-from .page.main_page import MainPage
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
     page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
+
+
+
 
 
